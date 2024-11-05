35609c748b119f84c54db4e8292f0e151439ce9c
Sophie Schaaf <Sophie.Schaaf@ConnectedSoftware.nl>
2024-11-05 12:43:05 +0100

    wip: Last change before moving to desktop

--- WebTmp/ModelStepper.cs
+++ WebTmp/ModelStepper.cs
@@ -3,20 +3,13 @@ using Services.Abstractions;
 
 namespace WebTmp;
 
-public class ModelStepper : BackgroundService
+public class ModelStepper(IEnumerable<IAgentService> agentServices) : BackgroundService
 {
-    private readonly IEnumerable<IAgentService> _agentServices;
-
-    public ModelStepper(IEnumerable<IAgentService> agentServices)
-    {
-        _agentServices = agentServices;
-    }
-
     private async Task InitializeModelAsync(CancellationToken cancellationToken)
     {
         // TODO: HubService & TruckCompanyService must have priority over the other Services
         
-        foreach (var agentService in _agentServices)
+        foreach (var agentService in agentServices)
         {
             await agentService.InitializeAgentsAsync(cancellationToken);
         }
@@ -24,7 +17,7 @@ public class ModelStepper : BackgroundService
     
     private async Task ExecuteStepAsync(CancellationToken cancellationToken)
     {
-        foreach (var agentService in _agentServices)
+        foreach (var agentService in agentServices)
         {
             await agentService.ExecuteStepAsync(cancellationToken);
         }
