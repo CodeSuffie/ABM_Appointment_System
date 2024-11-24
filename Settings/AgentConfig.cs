2651bc6c6d3a844e7f19d0a5bc9066723fb791c6
unknown <sophie2schaaf@gmail.com>
2024-11-24 16:37:11 +0100

    feat: Base logger added, most TODOs except LocationService TODOs are done

--- Settings/AgentConfig.cs
+++ Settings/AgentConfig.cs
@@ -1,37 +1,84 @@
 namespace Settings;
 
-public static class AgentConfig
+public abstract class IAgentConfig
 {
-    // AdminStaff
-    public const int AdminStaffCount = 9;
+    public int AdminStaffCount = 9;
     public const double AdminStaffAverageWorkDays = 0.7;
-
-    // AdminShift
     public static TimeSpan AdminShiftAverageLength = TimeSpan.FromHours(8);
-
-    // BayStaff
     public const int BayStaffCount = 9;
     public const double BayStaffAverageWorkDays = 0.7;
-
-    // BayShift
     public static TimeSpan BayShiftAverageLength = TimeSpan.FromHours(8);
-
-    // TruckCompany
     public const int TruckCompanyCount = 9;
-
-    // Truck
     public const int TruckCount = 9;
     public const int TruckAverageSpeed = 1;
-
-    // Hub
     public const int HubCount = 9;
     public const double HubAverageOperatingDays = 1;
-    
     public const int HubXSize = 9;
     public const int HubYSize = 4;
 
-    // ParkingSpot
     public static int[,] ParkingSpotLocations =
+    {
+        {0, 2},
+        {0, 3},
+        {1, 2},
+        {1, 3},
+        {2, 2},
+        {2, 3},
+        {3, 2},
+        {3, 3},
+        {4, 2}
+    };
+
+    public static int[,] BayLocations =
+    {
+        {0, 0}, 
+        {1, 0},
+        {2, 0},
+        {3, 0},
+        {4, 0},
+        {5, 0},
+        {6, 0},
+        {7, 0},
+        {8, 0}
+    };
+
+    public static TimeSpan OperatingHourAverageLength = TimeSpan.FromHours(12);
+    public const double DoubleTripChance = 0.2;
+    public const double PickupChance = 0.5;
+}
+
+public class AgentConfig : IAgentConfig
+{
+    // AdminStaff
+    public int AdminStaffCount = 9;
+    public double AdminStaffAverageWorkDays = 0.7;
+
+    // AdminShift
+    public TimeSpan AdminShiftAverageLength = TimeSpan.FromHours(8);
+
+    // BayStaff
+    public int BayStaffCount = 9;
+    public double BayStaffAverageWorkDays = 0.7;
+
+    // BayShift
+    public TimeSpan BayShiftAverageLength = TimeSpan.FromHours(8);
+
+    // TruckCompany
+    public int TruckCompanyCount = 9;
+
+    // Truck
+    public int TruckCount = 9;
+    public int TruckAverageSpeed = 1;
+
+    // Hub
+    public int HubCount = 9;
+    public double HubAverageOperatingDays = 1;
+    
+    public int HubXSize = 9;
+    public int HubYSize = 4;
+
+    // ParkingSpot
+    public int[,] ParkingSpotLocations =
     {
         {0, 2},
         {0, 3},
@@ -45,7 +92,7 @@ public static class AgentConfig
     };
     
     // Bay
-    public static int[,] BayLocations =
+    public int[,] BayLocations =
     {
         {0, 0}, 
         {1, 0},
@@ -59,9 +106,9 @@ public static class AgentConfig
     };
     
     // OperatingHour
-    public static TimeSpan OperatingHourAverageLength = TimeSpan.FromHours(12);
+    public TimeSpan OperatingHourAverageLength = TimeSpan.FromHours(12);
     
     // Trip
-    public const double DoubleTripChance = 0.2;
-    public const double PickupChance = 0.5;
+    public double DoubleTripChance = 0.2;
+    public double PickupChance = 0.5;
 }
--- Settings/ModelConfig.cs
+++ Settings/ModelConfig.cs
@@ -1,30 +1,37 @@
 ﻿namespace Settings;
 
-public static class ModelConfig
+public class ModelConfigBase
 {
-    // ModelConfig
-    public static readonly Random Random = new Random(2);
+    public Random Random = new();
+    public int MinutesPerHour;
+    
+    public TimeSpan ModelTime;
+    public TimeSpan ModelStep;
+    
+    public TimeSpan CheckInWorkTime;
+    public TimeSpan DropOffWorkTime;
+    public TimeSpan PickUpWorkTime;
+    public TimeSpan FetchWorkTime;
+    
+    public int MaxX;
+    public int MaxY;
+    public int MinDistanceBetween;
+}
 
-    public const int MinutesPerHour = 60;
+public class ModelConfig : ModelConfigBase
+{
+    public new Random Random = new Random(2);
+    public new int MinutesPerHour = 60;
     
-    public static TimeSpan ModelTime = TimeSpan.FromDays(7);
-    public static TimeSpan ModelStep = TimeSpan.FromMinutes(1);
+    public new TimeSpan ModelTime = TimeSpan.FromDays(7);
+    public new TimeSpan ModelStep = TimeSpan.FromMinutes(1);
     
-    // WorkType.CheckIn
-    public static TimeSpan CheckInWorkTime = TimeSpan.FromMinutes(9);
+    public new TimeSpan CheckInWorkTime = TimeSpan.FromMinutes(9);
+    public new TimeSpan DropOffWorkTime = TimeSpan.FromMinutes(9);
+    public new TimeSpan PickUpWorkTime = TimeSpan.FromMinutes(9);
+    public new TimeSpan FetchWorkTime = TimeSpan.FromMinutes(9);
     
-    // WorkType.DropOff
-    public static TimeSpan DropOffWorkTime = TimeSpan.FromMinutes(9);
-    
-    // WorkType.PickUp
-    public static TimeSpan PickUpWorkTime = TimeSpan.FromMinutes(9);
-    
-    // BayWork
-    public static TimeSpan FetchWorkTime = TimeSpan.FromMinutes(9);
-    
-    // Location
-    public const int MaxX = 200;
-    public const int MaxY = 200;
-    
-    public const int MinDistanceBetween = 9;
+    public new int MaxX = 200;
+    public new int MaxY = 200;
+    public new int MinDistanceBetween = 9;
 }
