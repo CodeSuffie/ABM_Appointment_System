edaca6ae9843d6dbb7c7e6b81ac49f7b07171487
unknown <sophie2schaaf@gmail.com>
2024-11-27 14:43:57 +0100

    feat: Removed Android from DesktopApp TargetFrameworks

--- Settings/WebConfig.cs
+++ Settings/WebConfig.cs
@@ -4,8 +4,8 @@ public static class WebConfig
 {
     // DbConnection
     // public const string Test = "Filename=D:\\Users\\Sophie\\Documents\\Files\\Study\\TPM\\Year3\\Bachelor Eind Project\\ABM_Appointment_System\\Database\\Models.db";
-    public const string Test = "Filename=\"X:\\source\\github\\ABM_Appointment_System\\Database\\Models.db\"";
-    // public const string DbConnectionString = "Filename=Database\\Models.db";
-    public const string DbConnectionString = Test;
+    // public const string Test = "Filename=\"X:\\source\\github\\ABM_Appointment_System\\Database\\Models.db\"";
+    public const string DbConnectionString = "Filename=D:\\Users\\Sophie\\Documents\\Files\\Study\\TPM\\Year3\\Bachelor Eind Project\\ABM_Appointment_System\\Database\\Models.db";
+    // public const string DbConnectionString = Test;
 }
 
