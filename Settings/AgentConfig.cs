10aa098ee50aa5acd9a450715760560125989f1b
unknown <sophie2schaaf@gmail.com>
2024-11-28 21:33:01 +0100

    fix: Truck definitions and removed 0,0 Bay

--- Settings/AgentConfig.cs
+++ Settings/AgentConfig.cs
@@ -75,10 +75,10 @@ public class AgentConfig : AgentConfigBase
     
     public override int[,] BayLocations { get; } =
     {
-        {0, 0}, 
+        //{0, 0}, 
         {1, 0},
         {2, 0},
-        //{3, 0},
+        {3, 0},
         //{4, 0},
         //{5, 0},
         //{6, 0},
